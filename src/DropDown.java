269146b55a4515adfa424c1663ea0cf3d9c75cf5
MelonGodTier <104312801+MelonGodTier@users.noreply.github.com>
2024-01-02 11:05:51 -0500

    This is a project that features many ways GUI can be used

--- src/DropDown.java
+++ src/DropDown.java
@@ -1,17 +1,17 @@
 import javax.swing.*;
 public class DropDown {
     public static void main(String[] args) {
-        String[] optionsToChoose = {"dogshit", "anzyshit", "tacobellshit", "mcshit", "takeoutshit"};
+        String[] optionsToChoose = {"American", "Chinese Food", "Japanese food", "Indian food"};
 
-        String getFavFruit = (String) JOptionPane.showInputDialog(
+        String getFavFood = (String) JOptionPane.showInputDialog(
                 null,
-                "What Shit you like the most?",
-                "Choose shit",
+                "What type of food you like the most?",
+                "Choose the type of food you like",
                 JOptionPane.QUESTION_MESSAGE,
                 null,
                 optionsToChoose,
                 optionsToChoose[3]);
 
-        System.out.println("Your chosen shit: " + getFavFruit);
+        System.out.println("Your chosen styleoffood: " + getFavFood);
     }
 }
